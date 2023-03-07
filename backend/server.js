d84c2971315061227fe9a0e3817ce4fba2b8d78e
IfeoluwaOkesola <atoyebiife@gmail.com>
2023-03-07 21:03:34 +0100

    create folder for backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const dbconnection = require('./config/connection');
 const cookieParser = require('cookie-parser')
 const { routeManager } = require('./routes/rts');
+const dbconnection = require('./config/connection');
+const cors = require('cors')
 dbconnection()
 dotenv.config()
 
@@ -10,6 +11,7 @@ const app = express()
 const port= process.env.PORT
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors())
 app.use('/', routeManager)
 app.listen(port, ()=>{
 console.log(`app listening on ${port}`)
