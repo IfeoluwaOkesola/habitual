30f5f321414c160e90c16781ef9354c895cee277
IfeoluwaOkesola <atoyebiife@gmail.com>
2023-03-09 15:43:38 +0100

    update database

--- backend/server.js
+++ backend/server.js
@@ -15,13 +15,7 @@ app.use(cors({credentials:true, origin: function (origin, callback) {
     console.log(`origin${origin} access granted`)
     callback(null, true)
 }}))
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-    next();
-  });
+
 app.use('/', routeManager)
 app.listen(port, ()=>{
 console.log(`app listening on ${port}`)
